381d3adb7f21ee54f090956a83d666d3b34576a6
fancn <fanyj@yikeer.com>
2017-02-23 10:16:49 +0800

    substract test case added

--- src/lib/sample.test.js
+++ src/lib/sample.test.js
@@ -1,8 +1,15 @@
-import { add } from './sample'
+import { add, substract } from './sample'
 
 test('add function should add two params', () => {
-    const expected = 10,
-        result = add(5, 5);
+    const expected = 10
+    const result = add(5, 5);
+
+    expect(result).toEqual(expected)
+})
+
+test('substract function should substract param one from param two', () => {
+    const expected = 10
+    const result = substract(15, 5);
 
     expect(result).toEqual(expected)
 })
