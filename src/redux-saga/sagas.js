1a70f41361ff98fc1caf9c3c79eef28581c54d86
William Yang <545792860@qq.com>
2017-03-16 09:41:08 +0800

    用take代替takeEvery

--- src/redux-saga/sagas.js
+++ src/redux-saga/sagas.js
@@ -1,48 +1,52 @@
-import { put, call, takeEvery } from 'redux-saga/effects'
+import { put, call, take } from 'redux-saga/effects'
 import request from './request'
 
 function* loadTodos () {
-  yield takeEvery('LOAD_TODOS', function* () {
+  while (true) {
+    yield take('LOAD_TODOS')
     yield put({type: 'START_LOADING'})
     const {success, result: todos} = yield call(request.getTodoList)
     if (success) {
       yield put({type: 'RECEIVE_TODOS', payload: todos})
     }
     yield put({type: 'STOP_LOADING'})
-  })
+  }
 }
 
 function* saveTodo () {
-  yield takeEvery('SAVE_TODO', function* ({payload: todo}) {
+  while (true) {
+    const { payload: todo } = yield take('SAVE_TODO')
     yield put({type: 'START_LOADING'})
     const {success, result: todos} = yield call(request.saveTodo, todo)
     if (success) {
       yield put({type: 'RECEIVE_TODOS', payload: todos})
     }
     yield put({type: 'STOP_LOADING'})
-  })
+  }
 }
 
 function* removeTodo () {
-  yield takeEvery('REMOVE_TODO', function* ({payload: todo}) {
+  while (true) {
+    const { payload: todo } = yield take('REMOVE_TODO')
     yield put({type: 'START_LOADING'})
     const {success, result: todos} = yield call(request.removeTodo, todo)
     if (success) {
       yield put({type: 'RECEIVE_TODOS', payload: todos})
     }
     yield put({type: 'STOP_LOADING'})
-  })
+  }
 }
 
 function* toggleComplate () {
-  yield takeEvery('TOGGLE_COMPLATE', function* ({payload: todo}) {
+  while (true) {
+    const { payload: todo } = yield take('TOGGLE_COMPLATE')
     yield put({type: 'START_LOADING'})
     const {success, result: todos} = yield call(request.toggleComplate, todo)
     if (success) {
       yield put({type: 'RECEIVE_TODOS', payload: todos})
     }
     yield put({type: 'STOP_LOADING'})
-  })
+  }
 }
 export default function* () {
   yield [
