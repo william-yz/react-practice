5a79bf45deea3fcf9b7c95a74d26442e60232362
William Yang <545792860@qq.com>
2017-03-20 11:40:21 +0800

    React.PureComponent with Redux

--- src/purecomponent/index.js
+++ src/purecomponent/index.js
@@ -1,53 +1,55 @@
 import React from 'react'
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
 import NonePure from './none-pure'
 import Pure from './pure'
 import PureOrNot from './pure-or-not'
 
-let next = 6
-class PureComponent extends React.Component {
-  constructor(...args) {
-    super(...args)
-    this.state = {
-      list: ['1', '2', '3', '4', '5'],
-    }
-  }
-
-  handleMutableClick = () => {
-    this.state.list.push(`${next}`)
-    next += 1
-    this.setState({
-      list: this.state.list,
-    })
-  }
-
-  handleImmutableClick = () => {
-    this.setState({
-      list: [...this.state.list, `${next}`],
-    })
-    next += 1
-  }
-
-  update = () => {
-    this.setState({
-      list: this.state.list,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleMutableClick}>Add Element mutable</button>
-        <button onClick={this.handleImmutableClick}>Add Element immutable</button>
-        <button onClick={this.update}>Update</button>
-
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <NonePure list={this.state.list} />
-          <Pure list={this.state.list} />
-          <PureOrNot list={this.state.list} />
-        </div>
-      </div>
-    )
+const reducer = (state = {
+  list: ['1', '2', '3', '4', '5'],
+  other: '',
+}, action) => {
+  switch (action.type) {
+    case 'MUTABLE':
+      state.list.push(action.payload)
+      return { ...state, list: state.list }
+    case 'IMMUTABLE':
+      return { ...state, list: [...state.list, action.payload] }
+    case 'UPDATE':
+      return { ...state, other: 'other' }
+    default:
+      return state
   }
 }
+const store = createStore(reducer)
+const next = 6
+const mapStateToProps = ({ list }) => ({
+  list,
+})
+const mapDispatchToProps = dispatch => ({
+  handleMutableClick: () => dispatch({ type: 'MUTABLE', payload: `${next}` }),
+  handleImmutableClick: () => dispatch({ type: 'IMMUTABLE', payload: `${next}` }),
+  update: () => dispatch({ type: 'UPDATE' }),
+})
+const PureComponent = connect(mapStateToProps, mapDispatchToProps)(
+  ({ handleMutableClick, handleImmutableClick, update, list }) => (
+    <div>
+      <button onClick={handleMutableClick}>Add Element mutable</button>
+      <button onClick={handleImmutableClick}>Add Element immutable</button>
+      <button onClick={update}>Update</button>
 
-export default PureComponent
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <NonePure list={list} />
+        <Pure list={list} />
+        <PureOrNot list={list} />
+      </div>
+    </div>
+))
+
+const App = () => (
+  <Provider store={store}>
+    <PureComponent />
+  </Provider>
+)
+
+export default App
