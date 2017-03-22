7ba5ec39e90a43320ee838a0e1141ed4439a14cf
William Yang <545792860@qq.com>
2017-03-22 14:50:33 +0800

    recur component

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Saga from './redux-saga'
 import PureComponent from './purecomponent'
 import MyRedux from './my-redux'
 import Echarts from './echarts'
+import RecurComponent from './recur-component'
 
 render(
   <Router history={browserHistory}>
@@ -19,7 +20,8 @@ render(
       <Route path="/saga" component={Saga} />
       <Route path="/purecomponent" component={PureComponent} />
       <Route path="/myredux" component={MyRedux} />
-      <Route path="/Echarts" component={Echarts} />
+      <Route path="/echarts" component={Echarts} />
+      <Route path="/recurcomponent" component={RecurComponent} />
 
     </Route>
   </Router>,
